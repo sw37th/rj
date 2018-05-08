db281ae9c9d68c8524f9c38a627f14d45657e17b
skywalker.37th@gmail.com <skywalker.37th@gmail.com>
2018-05-09 00:10:38 +0900

    関数名を変更、可読性を上げる修正

--- recordjob/rj_utils.py
+++ recordjob/rj_utils.py
@@ -1,8 +1,8 @@
+from datetime import datetime, timedelta
 import re
-import datetime
 import time
 
-def analyze_time(str_time):
+def rj_parse_time(s_time):
     """
     'HH:MM:SS' or 'HH:MM' or 秒数 or 'now'を引数として受け取り、
     当日00:00:00からの差分としてtimedeltaオブジェクトとして返す
@@ -11,77 +11,82 @@ def analyze_time(str_time):
     re_now = re.compile(r'now', re.I)
     time = None
 
-    if re.match(re_time, str_time):
+    if re.match(re_time, s_time):
         # 'HH:MM:SS' or 'HH:MM' or 秒数
-        t = list(map(int, str_time.split(':')))
+        t = list(map(int, s_time.split(':')))
         if len(t) == 3:
-            time = datetime.timedelta(seconds=(t[0]*60*60)+(t[1]*60)+t[2])
+            time = timedelta(seconds=(t[0] * 360) + (t[1] * 60) + t[2])
         elif len(t) == 2:
-            time = datetime.timedelta(seconds=(t[0]*60*60)+(t[1]*60))
+            time = timedelta(seconds=(t[0] * 360) + (t[1] * 60))
         elif len(t) == 1:
-            time = datetime.timedelta(seconds=t[0])
-    elif re.match(re_now, str_time):
+            time = timedelta(seconds=t[0])
+    elif re.match(re_now, s_time):
         # 'now'
-        current = datetime.datetime.now()
-        time = (datetime.timedelta(
-                seconds=(current.hour*60*60)+(current.minute*60)+current.second,
-                microseconds=current.microsecond))
+        n = datetime.now()
+        n_sec = (n.hour * 360) + (n.minute * 60) + n.second
+        time = timedelta(seconds=n_sec, microseconds=n.microsecond)
 
     return time
 
-def analyze_date(str_date):
+def rj_parse_date(s_date):
     """
-    'YYYY/MM/DD' or 'MM/DD' or 'DD' or 'sun|mon|tue|wed|thu|fri|sat'を引数として受け取り、
+    'YYYY/MM/DD|MM/DD|DD' or
+    'sun|mon|tue|wed|thu|fri|sat' or
+    'today' or
+    '+Nd'
+    を引数として受け取り、
     'YYYY/MM/DD 00:00:00'のdatetimeオブジェクトとして返す
     """
-    # "YYYY/MM/DD" or "MM/DD" or "DD" or "sun|mon|tue|wed|thu|fri|sat"
     re_wday = re.compile(r'^sun$|^mon$|^tue$|^wed$|^thu$|^fri$|^sat$', re.I)
     re_date = re.compile(r'^\d{4}/\d{1,2}/\d{1,2}$|^\d{1,2}/\d{1,2}$|^\d{1,2}$')
     re_today = re.compile(r'^today$', re.I)
     re_plus = re.compile(r'^\+(\d+)d$', re.I)
-
-    current = datetime.datetime.now()
-
     date = None
 
-    if re.match(re_wday, str_date):
-        # Weekday
-        wdaynum = {
-            'mon': 0,
-            'tue': 1,
-            'wed': 2,
-            'thu': 3,
-            'fri': 4,
-            'sat': 5,
-            'sun': 6,
-        }
-        today = current.weekday()
-        offset = wdaynum[str_date.lower()] - today
-        if offset < 0:
-            offset += 7
-        date = (datetime.datetime(current.year, current.month, current.day) +
-                datetime.timedelta(days=offset))
+    n = datetime.now()
 
-    elif re.match(re_date, str_date):
-        # YYYY/MM/DD or MM/DD or DD
-        d = str_date.split('/')
-        if len(d) == 3:
-            year, month, day = map(int, d)
-        elif len(d) == 2:
-            month, day = map(int, d)
-            year = current.year
-        else:
-            day = int(d[0])
-            year = current.year
-            month = current.month
-        date = datetime.datetime(year, month, day)
-    elif re.match(re_today, str_date):
-        # today
-        date = datetime.datetime(current.year, current.month, current.day)
-    elif re.match(re_plus, str_date):
-        # +n day
-        n = re.match(re_plus, str_date).group(1)
-        date = (datetime.datetime(current.year, current.month, current.day) +
-                datetime.timedelta(days=int(n)))
+    try:
+        if re.match(re_wday, s_date):
+            # Weekday
+            wdaynum = {
+                'mon': 0,
+                'tue': 1,
+                'wed': 2,
+                'thu': 3,
+                'fri': 4,
+                'sat': 5,
+                'sun': 6,
+            }
+            offset = wdaynum.get(s_date.lower()) - n.weekday()
+            if offset < 0:
+                # next week
+                offset += 7
+            date = datetime(n.year, n.month, n.day) + timedelta(days=offset)
+
+        elif re.match(re_date, s_date):
+            # YYYY/MM/DD or MM/DD or DD
+            d = s_date.split('/')
+            if len(d) == 3:
+                year, month, day = map(int, d)
+            elif len(d) == 2:
+                month, day = map(int, d)
+                year = n.year
+            else:
+                day = int(d[0])
+                year = n.year
+                month = n.month
+            date = datetime(year, month, day)
+
+        elif re.match(re_today, s_date):
+            # today
+            date = datetime(n.year, n.month, n.day)
+
+        elif re.match(re_plus, s_date):
+            # +n day
+            offset = int(re.match(re_plus, s_date).group(1))
+            date = datetime(n.year, n.month, n.day) + timedelta(days=offset)
+
+    except ValueError:
+        pass
 
     return date
