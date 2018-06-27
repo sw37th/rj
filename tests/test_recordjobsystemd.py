44b143092af7d9f49ff0c207531c200d83424e30
Akihiko Ota <skywalker.37th@gmail.com>
2018-06-28 01:05:16 +0900

    test_create_timer_calendar()をリピートフラグ毎にテストするよう変更

--- tests/test_recordjobsystemd.py
+++ tests/test_recordjobsystemd.py
@@ -11,29 +11,70 @@ class RecordJobSystemdTest(unittest.TestCase):
         rec = rjs.RecordJobSystemd()
         self.assertEqual(str(rec), 'RecordJobSystemd')
 
-    #@mock_open.patch('RecordJobSystemd._create_timer.open', m)
-    def test_create_timer_onshot(self):
-        rec = rjs.RecordJobSystemd()
+    def test_create_timer_calendar(self):
+        """
+        _create_timer()で作成されるtimerユニットファイルの
+        repeatフラグとOnCalendarフォーマットのペアが適正か
+        """
         begin = datetime(2018, 6, 30, 00, 00, 00)
+        repeats = {
+            'WEEKLY':  'Sat *-*-* 00:00:00',
+            'DAILY':   '*-*-* 00:00:00',
+            'WEEKDAY': 'Mon..Fri *-*-* 00:00:00',
+            'ASADORA': 'Mon..Sat *-*-* 00:00:00',
+            'ONESHOT': '2018-06-30 00:00:00',
+        }
         timer = """# created programmatically via rj. Do not edit.
 [Unit]
-Description=RJ:ONESHOT: timer unit for oneshot_timer
+Description=RJ:{_repeat}: timer unit for dummytimer
 CollectMode=inactive-or-failed
 
 [Timer]
 AccuracySec=1s
-OnCalendar=2018-06-30 00:00:00
+OnCalendar={_calendar}
 RemainAfterElapse=no
 
 [Install]
 WantedBy=timers.target
 """
-        m = mock_open()
-        with patch('recordjob.RecordJobSystemd.open', m, create=True):
-            rec._create_timer('test', 'oneshot_timer', begin, '')
-            #print(m.called)
-            #print(m.call_args)
-            mock = m()
-            #print(mock.write.call_args)
-            mock.write.assert_any_call(timer)
+        rec = rjs.RecordJobSystemd()
+        mopen = mock_open()
+        with patch('recordjob.RecordJobSystemd.open', mopen, create=True):
+            for rep, cal in repeats.items():
+                rec._create_timer('test', 'dummytimer', begin, rep)
+
+                f = mopen()
+                f.write.assert_any_call(
+                    timer.format(_repeat=rep, _calendar=cal)
+                )
+
+            # no repeat flag
+            rec._create_timer('test', 'dummytimer', begin)
+            f = mopen()
+            f.write.assert_any_call(
+                timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
+            )
+
+            # invalid repeat flag
+            rec._create_timer('test', 'dummytimer', begin, 'invalid')
+            f = mopen()
+            f.write.assert_any_call(
+                timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
+            )
+
+    def test_create_service_calendar(self):
+        """
+        _create_timer()で作成されるtimerユニットファイルの
+        repeatフラグとOnCalendarフォーマットのペアが適正か
+        """
+        service = """# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ: service unit for dummyservice
+CollectMode=inactive-or-failed
+
+[Service]
+Environment="RJ_ch=211" "RJ_walltime=1770"
+ExecStart=@/bin/bash "/bin/bash" "-c" "{_recpt1} $$RJ_ch $$RJ_walltime {_output}.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
+{_execstop}
+"""
 
