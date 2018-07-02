f7af4f8063e6228d18e573248e4790749918de4c
Akihiko Ota <skywalker.37th@gmail.com>
2018-07-02 23:58:49 +0900

    test_is_bs()、test_gen_unitname_jobid()を実装

--- tests/test_recordjobsystemd.py
+++ tests/test_recordjobsystemd.py
@@ -1,22 +1,28 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from recordjob import RecordJobSystemd as rjs
 from io import StringIO
 from unittest.mock import mock_open, patch
 from datetime import datetime, timedelta
 
 
+begin = datetime(2018, 6, 30, 00, 00, 00)
 
 class RecordJobSystemdTest(TestCase):
+    def setUp(self):
+        super(RecordJobSystemdTest, self).setUp()
+        self.rec = rjs.RecordJobSystemd()
+
+    def tearDown(self):
+        super(RecordJobSystemdTest, self).tearDown()
+
     def test_classname(self):
-        rec = rjs.RecordJobSystemd()
-        self.assertEqual(str(rec), 'RecordJobSystemd')
+        self.assertEqual(str(self.rec), 'RecordJobSystemd')
 
     def test_create_timer_calendar(self):
         """
         _create_timer()で作成されるtimerユニットファイルの
         repeatフラグとOnCalendarフォーマットのペアが適正か
         """
-        begin = datetime(2018, 6, 30, 00, 00, 00)
         repeats = {
             'WEEKLY':  'Sat *-*-* 00:00:00',
             'DAILY':   '*-*-* 00:00:00',
@@ -38,11 +44,10 @@ RemainAfterElapse=no
 [Install]
 WantedBy=timers.target
 """
-        rec = rjs.RecordJobSystemd()
         mopen = mock_open()
         with patch('recordjob.RecordJobSystemd.open', mopen, create=True):
             for rep, cal in repeats.items():
-                rec._create_timer('test', 'dummytimer', begin, rep)
+                self.rec._create_timer('test', 'dummytimer', begin, rep)
 
                 f = mopen()
                 f.write.assert_any_call(
@@ -50,27 +55,25 @@ WantedBy=timers.target
                 )
 
             # no repeat flag
-            rec._create_timer('test', 'dummytimer', begin)
+            self.rec._create_timer('test', 'dummytimer', begin)
             f = mopen()
             f.write.assert_any_call(
                 timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
             )
 
             # invalid repeat flag
-            rec._create_timer('test', 'dummytimer', begin, 'invalid')
+            self.rec._create_timer('test', 'dummytimer', begin, 'invalid')
             f = mopen()
             f.write.assert_any_call(
                 timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
             )
 
     def test_create_service_calendar(self):
-        #@mock.patch('recordjob.RecordJobSystemd')
         """
         _create_timer()で作成されるtimerユニットファイルの
         repeatフラグとOnCalendarフォーマットのペアが適正か
         """
-        rec = rjs.RecordJobSystemd()
-        unit = rec.unitdir + '/test.service'
+        unit = self.rec.unitdir + '/test.service'
         rectime = timedelta(seconds=1770)
         service_bs_repeat = """\
 # created programmatically via rj. Do not edit.
@@ -82,7 +85,7 @@ CollectMode=inactive-or-failed
 Environment="RJ_ch=211" "RJ_walltime=1770"
 ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
 
-""".format(_recdir=rec.recdir)
+""".format(_recdir=self.rec.recdir)
 
         service_bs_once = """\
 # created programmatically via rj. Do not edit.
@@ -94,7 +97,7 @@ CollectMode=inactive-or-failed
 Environment="RJ_ch=211" "RJ_walltime=1770"
 ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
 ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
-""".format(_recdir=rec.recdir)
+""".format(_recdir=self.rec.recdir)
 
         service_tt_repeat = """\
 # created programmatically via rj. Do not edit.
@@ -106,7 +109,7 @@ CollectMode=inactive-or-failed
 Environment="RJ_ch=15" "RJ_walltime=1770"
 ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
 
-""".format(_recdir=rec.recdir)
+""".format(_recdir=self.rec.recdir)
 
         service_tt_once = """\
 # created programmatically via rj. Do not edit.
@@ -118,31 +121,44 @@ CollectMode=inactive-or-failed
 Environment="RJ_ch=15" "RJ_walltime=1770"
 ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
 ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
-""".format(_recdir=rec.recdir)
+""".format(_recdir=self.rec.recdir)
 
         mopen = mock_open()
         with patch('recordjob.RecordJobSystemd.open', mopen, create=True):
-            rec._create_service(unit, '211', 'dummyservice', rectime)
+            self.rec._create_service(unit, '211', 'dummyservice', rectime)
             f = mopen()
             #print(f.write.call_args)
             f.write.assert_any_call(service_bs_once)
 
-            rec._create_service(unit, '211', 'dummyservice', rectime, 'ONESHOT')
+            self.rec._create_service(unit, '211', 'dummyservice', rectime, 'ONESHOT')
             f = mopen()
             f.write.assert_any_call(service_bs_once)
 
-            rec._create_service(unit, '211', 'dummyservice', rectime, 'WEEKLY')
+            self.rec._create_service(unit, '211', 'dummyservice', rectime, 'WEEKLY')
             f = mopen()
             f.write.assert_any_call(service_bs_repeat)
 
-            rec._create_service(unit, '15', 'dummyservice', rectime)
+            self.rec._create_service(unit, '15', 'dummyservice', rectime)
             f = mopen()
             f.write.assert_any_call(service_tt_once)
 
-            rec._create_service(unit, '15', 'dummyservice', rectime, 'ONESHOT')
+            self.rec._create_service(unit, '15', 'dummyservice', rectime, 'ONESHOT')
             f = mopen()
             f.write.assert_any_call(service_tt_once)
 
-            rec._create_service(unit, '15', 'dummyservice', rectime, 'WEEKLY')
+            self.rec._create_service(unit, '15', 'dummyservice', rectime, 'WEEKLY')
             f = mopen()
             f.write.assert_any_call(service_tt_repeat)
+
+    def test_is_bs(self):
+        # 64~  ... expect True
+        # 1-63 ... expect False
+        self.assertTrue(self.rec._is_bs(64))
+        self.assertFalse(self.rec._is_bs(63))
+
+    def test_gen_unitname_jobid(self):
+        unit, rj_id_long = self.rec._gen_unitname_jobid('15', 'test', begin)
+        expect_unit = 'RJ.15.test.20180630000000.tt'
+        expect_id = '6728ed28435be863a9ee453ecc391f2f6415aada2dc01d61643c2f25dac9f095'
+        self.assertEqual(expect_unit, unit)
+        self.assertEqual(expect_id, rj_id_long)
