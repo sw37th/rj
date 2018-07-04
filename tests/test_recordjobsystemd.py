09687dd84ed9ffcc26ef7f28871a99dc104dd92a
Akihiko Ota <skywalker.37th@gmail.com>
2018-07-04 22:28:10 +0900

    確認用のテキストを整理

--- tests/test_recordjobsystemd.py
+++ tests/test_recordjobsystemd.py
@@ -9,19 +9,76 @@ begin = datetime(2018, 6, 30, 00, 00, 00)
 rectime = timedelta(seconds=1770)
 unit_tt = 'RJ.15.test.20180630000000.tt'
 unit_tt_id = '6728ed28435be863a9ee453ecc391f2f6415aada2dc01d61643c2f25dac9f095'
-start_timer = [
+expect_sctl_start_timer = [
     'systemctl',
     '--user',
     'start',
     unit_tt + '.timer'
 ]
-enable_timer = [
+expect_sctl_enable_timer = [
     'systemctl',
     '--user',
     'enable',
     unit_tt + '.timer'
 ]
+expect_timer_file = """\
+# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ:{_repeat}: timer unit for dummytimer
+CollectMode=inactive-or-failed
 
+[Timer]
+AccuracySec=1s
+OnCalendar={_calendar}
+RemainAfterElapse=no
+
+[Install]
+WantedBy=timers.target
+"""
+expect_service_file_bs_repeat = """\
+# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ: service unit for dummyservice
+CollectMode=inactive-or-failed
+
+[Service]
+Environment="RJ_ch=211" "RJ_walltime=1770"
+ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
+
+"""
+expect_service_file_bs_once = """\
+# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ: service unit for dummyservice
+CollectMode=inactive-or-failed
+
+[Service]
+Environment="RJ_ch=211" "RJ_walltime=1770"
+ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
+ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
+"""
+expect_service_file_tt_repeat = """\
+# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ: service unit for dummyservice
+CollectMode=inactive-or-failed
+
+[Service]
+Environment="RJ_ch=15" "RJ_walltime=1770"
+ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
+
+"""
+expect_service_file_tt_once = """\
+# created programmatically via rj. Do not edit.
+[Unit]
+Description=RJ: service unit for dummyservice
+CollectMode=inactive-or-failed
+
+[Service]
+Environment="RJ_ch=15" "RJ_walltime=1770"
+ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
+ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
+"""
 
 class RecordJobSystemdTest(TestCase):
     def setUp(self):
@@ -46,20 +103,6 @@ class RecordJobSystemdTest(TestCase):
             'ASADORA': 'Mon..Sat *-*-* 00:00:00',
             'ONESHOT': '2018-06-30 00:00:00',
         }
-        timer = """\
-# created programmatically via rj. Do not edit.
-[Unit]
-Description=RJ:{_repeat}: timer unit for dummytimer
-CollectMode=inactive-or-failed
-
-[Timer]
-AccuracySec=1s
-OnCalendar={_calendar}
-RemainAfterElapse=no
-
-[Install]
-WantedBy=timers.target
-"""
         mopen = mock_open()
         with patch('recordjob.RecordJobSystemd.open', mopen, create=True):
             for rep, cal in repeats.items():
@@ -67,21 +110,23 @@ WantedBy=timers.target
 
                 f = mopen()
                 f.write.assert_any_call(
-                    timer.format(_repeat=rep, _calendar=cal)
+                    expect_timer_file.format(_repeat=rep, _calendar=cal)
                 )
 
             # no repeat flag
             self.rec._create_timer('test', 'dummytimer', begin)
             f = mopen()
             f.write.assert_any_call(
-                timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
+                expect_timer_file.format(
+                    _repeat='ONESHOT', _calendar=repeats['ONESHOT'])
             )
 
             # invalid repeat flag
             self.rec._create_timer('test', 'dummytimer', begin, 'invalid')
             f = mopen()
             f.write.assert_any_call(
-                timer.format(_repeat='ONESHOT', _calendar=repeats['ONESHOT'])
+                expect_timer_file.format(
+                    _repeat='ONESHOT', _calendar=repeats['ONESHOT'])
             )
 
     def test_create_service_calendar(self):
@@ -90,80 +135,67 @@ WantedBy=timers.target
         repeatフラグとOnCalendarフォーマットのペアが適正か
         """
         unit = self.rec.unitdir + '/test.service'
-        service_bs_repeat = """\
-# created programmatically via rj. Do not edit.
-[Unit]
-Description=RJ: service unit for dummyservice
-CollectMode=inactive-or-failed
-
-[Service]
-Environment="RJ_ch=211" "RJ_walltime=1770"
-ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
-
-""".format(_recdir=self.rec.recdir)
-
-        service_bs_once = """\
-# created programmatically via rj. Do not edit.
-[Unit]
-Description=RJ: service unit for dummyservice
-CollectMode=inactive-or-failed
-
-[Service]
-Environment="RJ_ch=211" "RJ_walltime=1770"
-ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip --lnb 15 $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.211.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
-ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
-""".format(_recdir=self.rec.recdir)
-
-        service_tt_repeat = """\
-# created programmatically via rj. Do not edit.
-[Unit]
-Description=RJ: service unit for dummyservice
-CollectMode=inactive-or-failed
-
-[Service]
-Environment="RJ_ch=15" "RJ_walltime=1770"
-ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
-
-""".format(_recdir=self.rec.recdir)
-
-        service_tt_once = """\
-# created programmatically via rj. Do not edit.
-[Unit]
-Description=RJ: service unit for dummyservice
-CollectMode=inactive-or-failed
-
-[Service]
-Environment="RJ_ch=15" "RJ_walltime=1770"
-ExecStart=@/bin/bash "/bin/bash" "-c" "recpt1 --b25 --strip $$RJ_ch $$RJ_walltime {_recdir}/dummyservice.15.`date +%%Y%%m%%d_%%H%%M.%%S`.$$$.ts"
-ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
-""".format(_recdir=self.rec.recdir)
-
+        recdir = self.rec.recdir
         mopen = mock_open()
+        repeats = ('WEEKLY', 'DAILY', 'WEEKDAY', 'ASADORA')
         with patch('recordjob.RecordJobSystemd.open', mopen, create=True):
-            self.rec._create_service(unit, '211', 'dummyservice', rectime)
+            # BS without repeat flag (the same behavior as ONESHOT)
+            self.rec._create_service(
+                unit, '211', 'dummyservice', rectime)
             f = mopen()
-            #print(f.write.call_args)
-            f.write.assert_any_call(service_bs_once)
+            f.write.assert_any_call(
+                expect_service_file_bs_once.format(_recdir=recdir))
 
-            self.rec._create_service(unit, '211', 'dummyservice', rectime, 'ONESHOT')
+            # BS with ONESHOT flag 
+            self.rec._create_service(
+                unit, '211', 'dummyservice', rectime, 'ONESHOT')
             f = mopen()
-            f.write.assert_any_call(service_bs_once)
+            f.write.assert_any_call(
+                expect_service_file_bs_once.format(_recdir=recdir))
 
-            self.rec._create_service(unit, '211', 'dummyservice', rectime, 'WEEKLY')
+            # BS with invalid flag (the same behavior as ONESHOT)
+            self.rec._create_service(
+                unit, '211', 'dummyservice', rectime, 'XXXXXXXX')
             f = mopen()
-            f.write.assert_any_call(service_bs_repeat)
+            f.write.assert_any_call(
+                expect_service_file_bs_once.format(_recdir=recdir))
 
-            self.rec._create_service(unit, '15', 'dummyservice', rectime)
-            f = mopen()
-            f.write.assert_any_call(service_tt_once)
+            # BS with each repeat flag
+            for rep in repeats:
+                self.rec._create_service(
+                    unit, '211', 'dummyservice', rectime, rep)
+                f = mopen()
+                f.write.assert_any_call(
+                    expect_service_file_bs_repeat.format(_recdir=recdir))
 
-            self.rec._create_service(unit, '15', 'dummyservice', rectime, 'ONESHOT')
+            # Terrestrial without repeat flag (the same behavior as ONESHOT)
+            self.rec._create_service(
+                unit, '15', 'dummyservice', rectime)
             f = mopen()
-            f.write.assert_any_call(service_tt_once)
+            f.write.assert_any_call(
+                expect_service_file_tt_once.format(_recdir=recdir))
 
-            self.rec._create_service(unit, '15', 'dummyservice', rectime, 'WEEKLY')
+            # Terrestrial with ONESHOT flag
+            self.rec._create_service(
+                unit, '15', 'dummyservice', rectime, 'ONESHOT')
             f = mopen()
-            f.write.assert_any_call(service_tt_repeat)
+            f.write.assert_any_call(
+                expect_service_file_tt_once.format(_recdir=recdir))
+
+            # Terrestrial with ONESHOT flag (the same behavior as ONESHOT)
+            self.rec._create_service(
+                unit, '15', 'dummyservice', rectime, 'XXXXXXXX')
+            f = mopen()
+            f.write.assert_any_call(
+                expect_service_file_tt_once.format(_recdir=recdir))
+
+            # Terrestrial with each repeat flag
+            for rep in repeats:
+                self.rec._create_service(
+                    unit, '15', 'dummyservice', rectime, rep)
+                f = mopen()
+                f.write.assert_any_call(
+                    expect_service_file_tt_repeat.format(_recdir=recdir))
 
     def test_is_bs(self):
         """
@@ -185,8 +217,17 @@ ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
         expect_unit_timer = self.rec.unitdir + '/' + unit_tt + '.timer'
         expect_unit_service = self.rec.unitdir + '/' + unit_tt + '.service'
         expect_calls_run = [
-            call(start_timer, check=True, stderr=STDOUT, stdout=DEVNULL),
-            call(enable_timer, check=True, stderr=STDOUT, stdout=DEVNULL),]
+            call(
+                expect_sctl_start_timer,
+                check=True,
+                stderr=STDOUT,
+                stdout=DEVNULL),
+            call(
+                expect_sctl_enable_timer,
+                check=True,
+                stderr=STDOUT,
+                stdout=DEVNULL),
+        ]
 
         self.rec._create_timer = MagicMock()
         self.rec._create_service = MagicMock()
@@ -198,3 +239,6 @@ ExecStop=@/bin/bash "/bin/bash" "-c" "systemctl --user disable test.timer"
         self.rec._create_service.assert_called_with(
             expect_unit_service, '15', 'test', rectime, 'WEEKLY')
         m_run.assert_has_calls(expect_calls_run)
+
+    #@patch('recordjob.RecordJobSystemd.run')
+    #def test_remove(self, m_run):
