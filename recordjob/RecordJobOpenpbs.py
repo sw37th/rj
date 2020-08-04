45c58db6e0fc5f612b5072eca6c0cc3a6f0bd7d0
Akihiko Ota <skywalker.37th@gmail.com>
2020-08-04 23:20:23 +0900

    add recordjob/RecordJobOpenpbs.py

---
+++ recordjob/RecordJobOpenpbs.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from subprocess import run, PIPE, STDOUT, DEVNULL, CalledProcessError
+import hashlib
+import os
+import recordjob
+import sys
+
+class RecordJobOpenpbs:
+    def __init__(self):
+        pbsexec = '/work/pbs/bin/'
+        self.qstat = [pbsexec + 'qstat', '-f', '-1']
+        self.pbsnodes = [pbsexec + 'pbsnodes', '-a']
+        self.qsub = [pbsexec + 'qsub']
+        self.qalter = [pbsexec + 'qalter', '-a' ]
+        self.comm_timeout = 10
+        self.recpt1_path = '/usr/local/bin/recpt1'
+        self.scriptdir = '/home/autumn/jobsh'
+        self.logdir = '/home/autumn/log'
+        self.recdir = '/home/autumn/rec'
+        self.channel_file = '/home/autumn/work/rj/channel.yml'
+        self.job_state = {
+            'C': "Completed",
+            'E': "Exiting",
+            'H': "on Hold",
+            'Q': "Queued",
+            'R': "Recording",
+            'T': "Moved",
+            'W': "Waiting",
+            'S': "Suspend",
+        }
+        self.queuename = {
+            'satellite': 'bs',
+            'terrestrial': 'tt',
+        }
