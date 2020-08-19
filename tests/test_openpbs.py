b230c63ea687624f012841d3617f895bbb0ae8ad
Akihiko Ota <skywalker.37th@gmail.com>
2020-08-19 21:37:49 +0900

    test_create_jobscript()を削除、test_add()を追加

--- tests/test_openpbs.py
+++ tests/test_openpbs.py
@@ -23,57 +23,61 @@ class RecordJobOpenpbsTest(TestCase):
         self.assertTrue(self.rec._is_bs(64))
         self.assertFalse(self.rec._is_bs(63))
 
-    """
-    現在時刻を2020年08月11日 00時00分00秒(1597071600)に固定
-    """
-    @freeze_time('2020-08-11 00:00:00')
-    def test_create_jobscript(self):
+    def test_add(self):
         """
-        ジョブスクリプトのフォーマットチェック
+        録画予約ジョブ用qsubコマンドオプションと
+        録画用recpt1コマンドオプションのチェック
         """
-        tt_name_expected = '/home/autumn/jobsh/tt_test.202008120134.30.1597071600.0.sh'
-        tt_body_expected = dedent('''\
-            #PBS -N 15.tt_test
-            #PBS -a 202008120134.30
-            #PBS -l walltime=1770
-            #PBS -l tt=1
-            #PBS -j oe
-            #PBS -o /home/autumn/log
-            #PBS -e /home/autumn/log
-            umask 022
-            _jobid=`echo $PBS_JOBID | awk -F'.' '{printf("%05d", $1)}'`
-            /usr/local/bin/recpt1 --b25 --strip 15 - /home/autumn/rec/tt_test.15.`date +%Y%m%d_%H%M.%S`.${_jobid}.ts
-            ''')
-        bs_name_expected = '/home/autumn/jobsh/bs_test.202008120134.30.1597071600.0.sh'
-        bs_body_expected = dedent('''\
-            #PBS -N 103.bs_test
-            #PBS -a 202008120134.30
-            #PBS -l walltime=1770
-            #PBS -l bs=1
-            #PBS -j oe
-            #PBS -o /home/autumn/log
-            #PBS -e /home/autumn/log
-            umask 022
-            _jobid=`echo $PBS_JOBID | awk -F'.' '{printf("%05d", $1)}'`
-            /usr/local/bin/recpt1 --b25 --strip --lnb 15 103 - /home/autumn/rec/bs_test.103.`date +%Y%m%d_%H%M.%S`.${_jobid}.ts
-            ''')
+        proc = MagicMock()
+        self.rec._run_command = MagicMock(return_value=proc)
 
-        begin = datetime(2020, 8, 12, 1, 34, 30)
-        rectime = timedelta(seconds=1770)
+        # 地上波
+        expected_qsub_tt = [
+            '/work/pbs/bin/qsub',
+            '-N', '15.test_tt',
+            '-a', '202008192029.30',
+            '-l', 'walltime=1770',
+            '-l', 'tt=1',
+            '-j', 'oe',
+            '-o', '/home/autumn/log',
+            '-W', 'umask=222',
+            '-']
+        expected_jobexec_tt = \
+            '/usr/local/bin/recpt1 --b25 --strip 15 - '\
+            '/home/autumn/rec/test_tt.15.'\
+            '$(date +%Y%m%d_%H%M.%S).${PBS_JOBID%.*}.ts'
+        proc.stdout = '110.example.org'
 
-        # 地上波用ジョブスクリプト作成
-        with patch('rjsched.RecordJobOpenpbs.open', mock_open()) as mopen:
-            name = self.rec._create_jobscript('15', 'tt_test', begin, rectime)
-            handle = mopen()
-            handle.write.assert_called_once_with(tt_body_expected)
-            self.assertEqual(name, tt_name_expected)
+        jid = self.rec.add('15', 'test_tt', datetime(2020, 8, 19, 20, 29, 30),
+            timedelta(seconds=1770))
 
-        # BS用ジョブスクリプト作成
-        with patch('rjsched.RecordJobOpenpbs.open', mock_open()) as mopen:
-            name = self.rec._create_jobscript('103', 'bs_test', begin, rectime)
-            handle = mopen()
-            handle.write.assert_called_once_with(bs_body_expected)
-            self.assertEqual(name, bs_name_expected)
+        self.rec._run_command.assert_called_with(
+            expected_qsub_tt, expected_jobexec_tt)
+        self.assertEqual(jid, '110')
+
+        # 衛星放送
+        expected_qsub_tt = [
+            '/work/pbs/bin/qsub',
+            '-N', '211.test_bs',
+            '-a', '202008192029.30',
+            '-l', 'walltime=1770',
+            '-l', 'bs=1',
+            '-j', 'oe',
+            '-o', '/home/autumn/log',
+            '-W', 'umask=222',
+            '-']
+        expected_jobexec_tt = \
+            '/usr/local/bin/recpt1 --b25 --strip --lnb 15 211 - '\
+            '/home/autumn/rec/test_bs.211.'\
+            '$(date +%Y%m%d_%H%M.%S).${PBS_JOBID%.*}.ts'
+        proc.stdout = '111.example.org'
+
+        jid = self.rec.add('211', 'test_bs', datetime(2020, 8, 19, 20, 29, 30),
+            timedelta(seconds=1770))
+
+        self.rec._run_command.assert_called_with(
+            expected_qsub_tt, expected_jobexec_tt)
+        self.assertEqual(jid, '111')
 
     """
     現在時刻を2020年08月16日 20時03分00秒(1597575780)に固定
