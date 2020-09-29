6b2eb4d08ce4c7211f0b7ed553627d5fb6942db4
Akihiko Ota <skywalker.37th@gmail.com>
2020-09-29 23:28:55 +0900

    rjsched基底クラス用のユニットテストを追加

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python3 -m unittest -v tests/test_openpbs.py
 python3 -m unittest -v tests/test_cliutil.py
+python3 -m unittest -v tests/test_init.py
---
+++ tests/test_init.py
@@ -0,0 +1,33 @@
+from textwrap import dedent
+from unittest import TestCase
+from unittest.mock import mock_open, patch, MagicMock
+import rjsched
+
+class RecordJobTest(TestCase):
+    def setUp(self):
+        super(RecordJobTest, self).setUp()
+        config = {
+            'recpt1_path': '/usr/local/bin/recpt1',
+            'recpt1ctl_path': '/usr/local/bin/recpt1ctl',
+            'recdir': '/home/dummy/rec',
+            'channel_file': '/home/dummy/.rj/channel.yml',}
+        self.rec = rjsched.RecordJob(config)
+        self.maxDiff = None
+
+    def tearDown(self):
+        super(RecordJobTest, self).tearDown()
+
+    def test_is_bs(self):
+        self.assertTrue(self.rec._is_bs(64))
+        self.assertFalse(self.rec._is_bs(63))
+
+    def test_get_channel_list(self):
+        yaml_channel = dedent("""\
+            15:       MX
+            211:      BS11
+        """)
+        expected = {'15': 'MX', '211': 'BS11'}
+        mopen = mock_open(read_data=yaml_channel)
+        with patch('rjsched.open', mopen):
+            result = self.rec.get_channel_list()
+            self.assertEqual(result, expected)
