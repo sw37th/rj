5d5f82cd4a591826290096515e31f4db525a6db5
Akihiko Ota <skywalker.37th@gmail.com>
2021-07-16 01:11:31 +0900

    オプションを変数に退避

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-python3 -m unittest -v tests/test_openpbs.py
-python3 -m unittest -v tests/test_cliutil.py
-python3 -m unittest -v tests/test_init.py
+#_opt="-v"
+_opt=""
+python3 -m unittest ${_opt} tests/test_openpbs.py
+python3 -m unittest ${_opt} tests/test_cliutil.py
+python3 -m unittest ${_opt} tests/test_init.py
