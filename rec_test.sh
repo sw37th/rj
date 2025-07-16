dd7fcd746b61207e39f69cc39cd8ac941f1817b1
Akihiko Ota <skywalker.37th@gmail.com>
2025-07-16 22:36:17 +0900

    テスト用の録画時間をデフォルト30分に変更

--- rec_test.sh
+++ rec_test.sh
@@ -3,12 +3,15 @@
 _tt_start=$(date +"%H:%M" -d "2 minute"):00
 _bs_start=$(date +"%H:%M" -d "3 minute"):00
 
-./rj add 30 test_chiba today ${_tt_start} 00:01:00
-./rj add 26 test_nhke  today ${_tt_start} 00:01:00
-./rj add 27 test_nhkg  today ${_tt_start} 00:01:00
-./rj add 25 test_ntv   today ${_tt_start} 00:01:00
+#_rectime="00:01:00"
+_rectime=""  # default 00:29:30
 
-./rj add 101 test_nhkbs  today ${_bs_start} 00:01:00
-./rj add 161 test_bstbs  today ${_bs_start} 00:01:00
-./rj add 211 test_bs11   today ${_bs_start} 00:01:00
-./rj add 222 test_twelve today ${_bs_start} 00:01:00
+./rj add 22 test_tbs   today ${_tt_start} ${_rectime}
+./rj add 25 test_ntv   today ${_tt_start} ${_rectime}
+./rj add 26 test_nhke  today ${_tt_start} ${_rectime}
+./rj add 27 test_nhkg  today ${_tt_start} ${_rectime}
+
+./rj add 101 test_nhkbs  today ${_bs_start} ${_rectime}
+./rj add 161 test_bstbs  today ${_bs_start} ${_rectime}
+./rj add 211 test_bs11   today ${_bs_start} ${_rectime}
+./rj add 222 test_twelve today ${_bs_start} ${_rectime}
